84ade3627bd00349da441331fe22811d302458dc
Imanda03 <asis03ktm@gmail.com>
2022-08-23 13:35:25 +0545

    lets practice python

---
+++ practice]/gettingInput.py
@@ -0,0 +1,4 @@
+name = input('What is your name? ')
+color = input("Your favourite color: ")
+
+print(name + " Likes " +color);
---
+++ practice]/hello1.py
@@ -0,0 +1,13 @@
+msg = "Hello anish! Now start python...";
+
+print(msg)
+
+
+print("*" * 10)
+
+price = 10              
+rating = 3.5            
+name = 'Mosh'           
+is_published = False    
+
+print( name )
