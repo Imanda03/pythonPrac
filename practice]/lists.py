66071d95d8d6ddaca56083c32655b5d80910d8d5
Imanda03 <asis03ktm@gmail.com>
2022-08-29 01:05:05 +0545

    day 4

---
+++ practice]/lists.py
@@ -0,0 +1,33 @@
+#List is similar to array......
+
+mylist = []
+mylist.append(11)    #append la list ma first index bata haldai janxa
+mylist.append(22)
+mylist.append(33)
+
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+# x ma mylist ko sabbai halya ko
+for x in mylist:
+    print(x)
+
+#accessing list same as a array
+allList = [44, 55, 66]
+print(allList)
+
+
+
+#Exercises
+numbers = [1, 2, 3]
+strings = ["hello", 'world']
+names = ["anish", "sugam"]
+
+#taking index 2 of names
+secondIndex = names[1]
+
+print(numbers)
+print(strings)
+
+print('This is the second index of names:'+secondIndex);
